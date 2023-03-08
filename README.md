ab198ad62f6c44376d93e2a178885ddbb5f84219
Dhanu1402 <dhanukumar2000@gmail.com>
2023-03-08 12:52:32 +0530

    Layout structure

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Learnings
 
 1. Create next-app
+
    1. npx create-next-app .
    2. install tailwindcss postcss autoprefixer
       1. npx install tailwindcss postcss autoprefixer
@@ -10,3 +11,6 @@
       3. update tailwind.config.js
       4. update global.css
       5. npm run dev
+
+2. Layout structure
+   1. add colors in tailwind.config.js
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import Card from '../components/Card';
+
 export default function Home() {
-  return <div className="text-emerald-500">test</div>;
+  return (
+    <div className="flex mt-4 max-w-4xl mx-auto gap-6">
+      <div className="w-1/3">
+        <Card>Test links</Card>
+      </div>
+      <div className="grow">
+        <Card>Form here</Card>
+        <Card>First post test</Card>
+      </div>
+    </div>
+  );
 }
